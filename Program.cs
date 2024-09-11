fe6d809e8d318393106c69ac1d1df539a98a32ce
iszyperski <ignacy.szyperski.2009@gmail.com>
2024-09-11 15:14:32 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class program()
+{
+    public static int Main()
+    {
+        Random RNG = new Random();
+        int[] board = new int[100];
+        for (int i = 0; i < 100; i++)
+        {
+            board[i] = RNG.Next(-100, 100);
+        }
+        foreach (var a in board)
+        {
+            Console.WriteLine(a);
+        }
+  System.Console.WriteLine("reverse");      
+		for (int i = board.Length -1; i >= 0; i--)
+        {
+            Console.WriteLine(board[i]);
+        }
+		foreach(var a in board){
+			if(a > 0)
+			System.Console.WriteLine(a);
+		}
+
+		foreach(var a in board){
+			if(a < 0)
+			System.Console.WriteLine(a);
+		}
+		System.Console.WriteLine('e');
+		foreach(var a in board){
+			if(a > 0)
+			System.Console.WriteLine(a);
+		}
+
+		foreach(var a in board){
+			if(a < 0)
+			System.Console.WriteLine(a);
+		}
+			int suma = 0;
+		for(int a = 0 ; a < board.Length; a++){
+		suma+=board[a];	
+		}
+		System.Console.WriteLine(suma);
+		int avg(int[] beard)
+		 return 0;
+            }
+}
